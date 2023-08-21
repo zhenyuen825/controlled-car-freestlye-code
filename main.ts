2d3a17d3a5c71cdca71d26a265ed142b068e769e
zhenyuen825 <142199521+zhenyuen825@users.noreply.github.com>
2023-08-21 17:49:54 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+radio.onReceivedString(function (receivedString) {
+    item = receivedString
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    }
+})
+let item = ""
+radio.setGroup(1)
+radio.setTransmitPower(7)
+let flag1 = 0
+basic.showIcon(IconNames.Heart)
